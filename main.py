e0e8704327e47749071ae102fa0618962ee6bbd8
Hudrolax <hudro795@gmail.com>
2020-07-23 15:48:44 +0300

    bugfix

--- main.py
+++ main.py
@@ -3,15 +3,22 @@ import serial.tools.list_ports as lp
 
 class BarScanner:
     def __init__(self, port):
-        self.port = serial.Serial(port, 115200, timeout=1)
+        self._initialized = False
+        _ports = list(lp.comports())
+        for _port in _ports:
+            if _port.device == port:
+                self.port = serial.Serial(port.device, 115200)
+                self._initialized = True
+        if not self._initialized:
+            raise Exception(f'ERROR!!! Port {port} does not exist')
 
     def watch_port(self):
         while True:
             _answer = self.port.readln()
 
 if __name__ == '__main__':
+    com_port = 'COM3'
     print('hello')
-    print(serial.tools.list_ports)
     scanner = BarScanner('COM3')
     _answer = scanner.port.readline()
     print(_answer)
