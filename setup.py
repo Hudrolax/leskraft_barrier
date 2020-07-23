b99e5c886a354630af06887e5a73d613ef0fa520
Hudrolax <hudro795@gmail.com>
2020-07-23 15:29:03 +0300

    bugfix

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import subprocess
 
 if __name__ == '__main__':
     if os.path.exists('requirements.txt'):
-        subprocess.call(['pip', 'install', '-r requirements.txt'])
+        subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
     else:
         print('requirements.txt not found')
