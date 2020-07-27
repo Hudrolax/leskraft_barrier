384253387053b493fd64f0ed8a4dc8f424bc8a14
Hudrolax <hudro795@gmail.com>
2020-07-27 21:46:22 +0300

    update requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 import os.path
 import subprocess
+from sys import platform
 
 if __name__ == '__main__':
     if os.path.exists('requirements.txt'):
-        subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
+        if platform == "win32":
+            subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
+        else:
+            subprocess.call(['pip3', 'install', '-r', 'requirements.txt'])
     else:
         print('requirements.txt not found')
