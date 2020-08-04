c272fb2e04e042f465d90d6e91d7813b21fa4661
Hudrolax <hudro795@gmail.com>
2020-08-04 17:27:05 +0300

    add .env

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ LOG_LEVEL = logging.INFO
 if __name__ == '__main__':
     dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     if os.path.exists(dotenv_path):
-        print('.env loaded')
+        print(f'{dotenv_path} loaded')
         load_dotenv(dotenv_path)
     else:
         logging.critical('File .env not found!')
