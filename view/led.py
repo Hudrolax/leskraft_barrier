5938d9d9281a5a6a91d9ed1c48b7e3af92078e63
Hudrolax <hudro795@gmail.com>
2020-08-09 21:37:47 +0300

    bugfix

--- view/led.py
+++ view/led.py
@@ -1,5 +1,4 @@
 from RPi import GPIO
-from utility.pizypwm import *
 from utility.logger_super import LoggerSuper
 from utility.observer import Observer
 import threading
@@ -33,9 +32,6 @@ class LED:
     def blink_fast(self):
         self._mode = 3
 
-    def pwm_mode(self):
-        self._mode = 4
-
     def _led_on(self):
         GPIO.output(self._gpio, True)
 
@@ -58,22 +54,6 @@ class LED:
                 sleep(0.25)
                 self._led_off()
                 sleep(0.25)
-            elif self._mode == 4:
-                led_pwm = PiZyPwm(100, self._gpio, GPIO.BCM)
-                led_pwm.start(100)
-                dim_up = range(0, 80)
-                dim_down = range(80, 0)
-                while self._mode == 4:
-                    for power in dim_up:
-                        led_pwm.changeDutyCycle(power)
-                        sleep(0.01)
-                        if self._mode != 4:
-                            break
-                    for power in dim_down:
-                        led_pwm.changeDutyCycle(power)
-                        sleep(0.01)
-                        if self._mode != 4:
-                            break
 
 
 class LedAssembly(Observer, LoggerSuper):
@@ -88,11 +68,12 @@ class LedAssembly(Observer, LoggerSuper):
 
     def model_is_changed(self):
         if self.model.permission:
+            self.red_led.led_off()
             self.green_led.blink_fast()
         else:
             self.green_led.led_off()
 
-        if self.model.connected:
-            self.red_led.pwm_mode()
-        else:
-            self.red_led.blink()
+            if self.model.connected:
+                self.red_led.blink()
+            else:
+                self.red_led.led_off()
