b1da79a84d8f181ab77a64539d9f4c4a71a27bbf
Hudrolax <hudro795@gmail.com>
2020-08-09 21:14:31 +0300

    bugfix

--- test.py
+++ test.py
@@ -1,13 +1,31 @@
 from view.led import LedAssembly
 from time import sleep
+from RPi import GPIO
 
 if __name__ == '__main__':
-    led = LedAssembly(None)
-    led.green_led.led_on()
-    sleep(0.5)
-    led.green_led.led_off()
-    sleep(0.5)
-    led.green_led.led_on()
-    sleep(0.5)
-    led.green_led.led_off()
-    led.green_led.blink_fast()
+    GPIO.setmode(GPIO.BCM)  # говорим о том, что мы будем обращаться к контактам по номеру канала
+    GPIO.setup(3, GPIO.OUT)  # Настраиваем GPIO пин на вывод
+    GPIO.output(3, True)
+    sleep(1)
+    GPIO.output(3, False)
+    sleep(1)
+    GPIO.output(3, True)
+    sleep(1)
+    GPIO.output(3, False)
+    sleep(1)
+    GPIO.output(3, True)
+    sleep(1)
+    GPIO.output(3, False)
+    sleep(1)
+    GPIO.output(3, True)
+    sleep(1)
+    GPIO.output(3, False)
+    sleep(1)
+    GPIO.output(3, True)
+    sleep(1)
+    GPIO.output(3, False)
+    sleep(1)
+    GPIO.output(3, True)
+    sleep(1)
+    GPIO.output(3, False)
+    sleep(1)
